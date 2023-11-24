d18ccfc4767deb40af8242f8f0f8bd41cd3f55c8
Лев Антонов <jackmar133@gmail.com>
2023-11-24 15:47:40 +0500

    Создание контекста данных, написание строки подключения к любому экземпляру MS SQL Server, связи контекста и строки в файле Program

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoParts.Models
+{
+    public class User : IdentityUser
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+
+        public string NameUser { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [Display(Name = "Пароль")]
+
+        public string Pass { get; set; }
+
+        [Required(ErrorMessage = "Введите номер телефона")]
+        [Display(Name = "Телефон")]
+
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Введите email")]
+        [Display(Name = "Email")]
+
+        public string Email { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using AutoParts.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppCtx>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<AppCtx>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
